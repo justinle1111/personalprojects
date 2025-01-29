244f04d6dcf444f513c0718413c65886c13e3a6f
Justin Le <lej42@uci.edu>
2025-01-29 09:17:44 -0800

    Changed Neuron.py to Neuron.ipynb

--- neuron.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Neuron:
-    def __init__(self, num_inputs):
-        self.weights = np.random.randn(num_inputs) * 0.01
-        self.bias = 0
-
-    def forward_propagation(self, weights):
-        results = np.dot(self.weight, inputs) + self.bias
